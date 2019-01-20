2ff111577b8a40fa52cfd4e1596f4507db886489
Lars Boegild Thomsen <lth@netcompartner.com>
2019-01-20 11:15:12 +0800

    Init kind of working

--- background.js
+++ background.js
@@ -7,8 +7,21 @@
         numericValue: 1000, 
         stringValue: "Testing 1 2 3", 
         booleanValue: true, 
+        mixedArray: [
+            "String 1", 
+            "String 2", 
+            42
+        ], 
         testFunction: function() {
             console.log("testFunction call");
+        }, 
+        utils: {
+            a: function() {
+                console.log("utils.a call");
+            }, 
+            b: function() {
+                console.log("utils.b call");
+            }
         }
     };
 
--- rpc-router.js
+++ rpc-router.js
@@ -1,13 +1,72 @@
 /*
  * Extension RPC Router
  */
+
+/**
+ * Recursively traverses object and build a descriptor
+ * @param {*} name 
+ * @param {*} object 
+ */
+function traverse(name, object) { 
+    var objectType = typeof object;
+    console.log((name || "Root") + " object type: ", objectType);
+
+    var descriptor;
+
+    if (objectType === "object" && Array.isArray(object)) {
+        descriptor = [];
+        Object.keys(object).forEach(function(key) {
+            var subDescriptor = traverse(key, object[key]);
+            descriptor[key] = subDescriptor;
+        });
+    } else if (objectType === "object") {
+        descriptor = {};
+        Object.keys(object).forEach(function(key) {
+            var subDescriptor = traverse(key, object[key]);
+            descriptor[key] = subDescriptor;
+        });
+    } else {
+        descriptor = {
+            type: objectType, 
+            value: object
+        };
+    }
+
+    console.log((name || "Root") + " returning: ", descriptor);
+
+    return descriptor;
+
+}
+
 function ExtensionRpcServer(id, object) {
     this.id = id;
     this.object = object;
+
+    this.descriptor = traverse("", object);
+
+    chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+            if (request.name && request.name === id) {
+                if (request.type === "init") {
+                    sendResponse(this.descriptor);
+                } else {
+                    console.log("Ignoring message of type ", request.type);
+                }
+            } else {
+                console.log("Ignoring message from ", sender);
+            }
+        }
+    );
+
 }
 
 function ExtensionRpcClient(id) {
     this.id = id;
+
+    chrome.runtime.sendMessage({name: id, type: "init"}, function(response) {
+        console.log("Response: ", response);
+    });
+
 }
 
 /*
